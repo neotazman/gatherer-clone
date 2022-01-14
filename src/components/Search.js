672d8f18d3e2043d7b2677d11399a630b927f973
Tristan Alie <TristanAlie@yahoo.com>
2022-01-14 01:35:24 -0500

    added mtgsdk, but can't access it correctly

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 
 export default function Search() {
 
+    // const mtg = require('mtgsdk')
+
     const initialState = {
         cardName: '',
         type: '',
@@ -14,17 +16,24 @@ export default function Search() {
     const [searchBoxes, setSearchBoxes] = useState(initialState)
 
     const handleInputChange = (e) => {
+        e.preventDefault()
         setSearchBoxes({ ...searchBoxes, [e.target.name]: e.target.value })
+    }
+
+    const consoleLogError = (e) => {
+        e.preventDefault()
+        console.log(searchBoxes)
     }
 
     return (
         <div>
-            <input type="text" placeholder='Search Card Name' name='cardName' value={searchBoxes.cardName} onChange={handleInputChange}/>
+            <input type="text" placeholder='Search Card Name' name='cardName' value={searchBoxes.cardName} onChange={handleInputChange} />
             <input type="text" placeholder='Search Type' name='type' value={searchBoxes.type} onChange={handleInputChange} />
             <input type="text" placeholder='Search Subtype' name='subtype' value={searchBoxes.subtype} onChange={handleInputChange} />
             <input type="text" placeholder='Search Set' name='set' value={searchBoxes.set} onChange={handleInputChange} />
             <input type="text" placeholder='Search Text' name='text' value={searchBoxes.text} onChange={handleInputChange} />
             <input type='number' placeholder='Search Mana Value' name='cmc' value={searchBoxes.cmc} onChange={handleInputChange} />
+            <button onClick={consoleLogError}>Console Error</button>
         </div>
     )
 }
